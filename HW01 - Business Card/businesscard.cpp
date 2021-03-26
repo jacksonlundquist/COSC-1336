65cd9a587025929794622c376541dbd8c3cdc5df
Jackson Lundquist <81322691+jacksonlundquist@users.noreply.github.com>
2021-03-26 12:33:35 -0500

    Upload business card homework exercise

---
+++ HW01 - Business Card/businesscard.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Jackson L" << std::endl;
+    std::cout << "C++ Business Cards, LLC. - The Industry Leader in C++ Business Cards" << std::endl;
+    std::cout << "3535 N Ellison Dr, San Antonio, TX 78251" << std::endl;
+    std::cout << "(210) 555-5128" << std::endl;
+    std::cout << "https://crouton.net/" << std::endl;
+
+    return 0;
+}
