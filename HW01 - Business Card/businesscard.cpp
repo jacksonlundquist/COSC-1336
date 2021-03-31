54803c9f335d96be7cb09ed60db67f10cad1aa33
Jackson Lundquist <81322691+jacksonlundquist@users.noreply.github.com>
2021-03-30 19:16:45 -0500

    Update businesscard.cpp

--- HW01 - Business Card/businesscard.cpp
+++ HW01 - Business Card/businesscard.cpp
@@ -5,7 +5,6 @@ int main() {
     std::cout << "C++ Business Cards, LLC. - The Industry Leader in C++ Business Cards" << std::endl;
     std::cout << "3535 N Ellison Dr, San Antonio, TX 78251" << std::endl;
     std::cout << "(210) 555-5128" << std::endl;
-    std::cout << "https://crouton.net/" << std::endl;
 
     return 0;
 }
